a409989a0a36d696a5cd2ff0fdbc376c2e567a68
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2022-11-15 10:40:16 -0500

    V.1.5.1

--- src/main/java/com/SpringData/SpringData/web/SecuritySpring.java
+++ src/main/java/com/SpringData/SpringData/web/SecuritySpring.java
@@ -5,11 +5,15 @@
 package com.SpringData.SpringData.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 /**
@@ -20,19 +24,36 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 //clase para configurar los usuarios que se van a utilizar
 public class SecuritySpring extends WebSecurityConfigurerAdapter{
-    //metodo para agregar mas usuarios y configurarlos para hacer login el la aplicacion
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth){
+//    metodo para agregar mas usuarios y configurarlos para hacer login el la aplicacion
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth){
+//        try {
+//              auth.inMemoryAuthentication().withUser("admin")
+//                    .password("{noop}135")
+//                    .roles("ADMIN","USER")
+//                    .and()
+//                    .withUser("user")
+//                    .password("{noop}1356").roles("USER");
+//        } catch (Exception e) {
+//        }
+//    } 
+    
+    @Autowired
+    private UserDetailsService uds;
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+    
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build){
         try {
-              auth.inMemoryAuthentication().withUser("admin")
-                    .password("{noop}135")
-                    .roles("ADMIN","USER")
-                    .and()
-                    .withUser("user")
-                    .password("{noop}1356").roles("USER");
+             build.userDetailsService(uds).passwordEncoder(passwordEncoder());
         } catch (Exception e) {
         }
-    } 
+    }
+    
       //metodo para autorizacion de los usuarios, que pueden hacer
     @Override
     protected void configure(HttpSecurity http){
